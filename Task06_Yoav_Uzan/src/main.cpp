b51df9b659ff3f946427c09d05319753c8bfa1ce
yoav uzan <yoav547@gmail.com>
2023-06-20 00:01:40 +0300

    finish of the day

--- Task06_Yoav_Uzan/src/main.cpp
+++ Task06_Yoav_Uzan/src/main.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <vector>
 #include <algorithm>
 #include <cmath>
@@ -60,40 +61,50 @@ int main() {
             std::cout << "Please enter the wanted radius :";
             std::cin >> radius;
             Location& city_location = it->second;
-        //    float lat_loc= city_location._latitude;
-        //    float long_loc = city_location._longitude;
-        //    std::multimap<std::string, Location > matchingElements;
-        //    // copy all the elements that are in the square with the wanted radius
-        //    std::copy_if(myMap.begin(), myMap.end(), std::back_inserter(matchingElements),
-        //         [long_loc,lat_loc, radius](const std::pair<std::string, Location>& element)
-        //            {
-        //             return std::abs(element.second._latitude - lat_loc) <= radius 
-        //                 && std::abs(element.second._longitude - long_loc) <= radius;
-        //         });
-        //     std::cout << "Please enter the wanted norm (0 – L2, Euclidean distance, 1 – Linf, Chebyshev distance, 2 – L1, Manhattan distance):";
-        //     int norm;
-        //     std::cin >> norm;
-        //     map<int,float (*)(const Location&, const Location&) > choose_norm;
-        //     choose_norm[0] = L2;
-        //     choose_norm[1] = Linf;
-        //     choose_norm[2] = L1;
-        //     std::remove_if(matchingElements.begin(), matchingElements.end(),
-        //         [city_location, radius, norm, choose_norm]
-        //         (const std::pair<std::string, Location>& element)
-                 //{
-                    // return choose_norm.at(norm)(city_location, element.second) > radius;
-                 //});
-        //     std::cout<< matchingElements.size()<< " city/cities found in the given radius." << std::endl;
-        //     int count = std::count_if(matchingElements.begin(), matchingElements.end()
-        //         ,[long_loc]
-        //            (const std::pair<std::string, Location>& element)
-                 //   {
-                    // return element.second._longitude - long_loc > 0;
-                 //    });
-        //     std::cout<<count<<"cities are to the north of the selected city."<<std::endl;
-        //     std::cout<<"city list:"<<std::endl;
-        //     
-        }
+            float lat_loc= city_location._latitude;
+            float long_loc = city_location._longitude;
+            std::multimap<std::string, Location > matchingElements;
+            // copy all the elements that are in the square with the wanted radius
+            std::copy_if(myMap.begin(), myMap.end(), std::inserter(matchingElements, matchingElements.end()),
+                 [long_loc,lat_loc, radius]
+                 (const std::pair<const std::string, Location>& element)
+                    {
+                     return std::abs(element.second._latitude - lat_loc) <= radius 
+                         && std::abs(element.second._longitude - long_loc) <= radius;
+                 });
+            std::cout << "Please enter the wanted norm (0 – L2, Euclidean distance, 1 – Linf, Chebyshev distance, 2 – L1, Manhattan distance):";
+            int norm;
+            std::cin >> norm;
+            map<int, std::function <float (const Location&, const Location&)> > choose_norm;
+            choose_norm[0] = L2;
+            choose_norm[1] = Linf;
+            choose_norm[2] = L1;
+            std::multimap<std::string,Location > sortedMatches;
+            std::copy_if(matchingElements.begin(), matchingElements.end(),
+                std::inserter(sortedMatches, sortedMatches.end()),
+                [&city_location, &radius, &norm, &choose_norm]
+                (std::pair<const std::string, Location>& element) 
+                {
+                    return choose_norm[norm](city_location, element.second) < radius;
+                });
+        
+             std::cout<< sortedMatches.size()<< " city/cities found in the given radius." << std::endl;
+             int count = std::count_if(sortedMatches.begin(), sortedMatches.end()
+                 ,[long_loc]
+                    (const std::pair<std::string, Location>& element)
+                    {
+                     return (element.second._longitude - long_loc) > 0;
+                     });
+             std::cout<<count<<"cities are to the north of the selected city."<<std::endl;
+             std::cout<<"city list:"<<std::endl;
+             
+            std::transform(matchingElements.begin(), matchingElements.end()
+                    , std::ostream_iterator<std::string>(std::cout, "\n"),
+                [](const std::pair<const std::string, Location>& element) {
+                    return element.first;
+                });
+        
+    }
         std::cout << "Bye";
     
 
